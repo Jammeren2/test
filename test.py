a6f6496d3518bd4a1fa70ee1624e0c2573cff47e
Jammeren2 <52398081+Jammeren2@users.noreply.github.com>
2025-02-27 09:12:16 +0700

    Update test.py

--- test.py
+++ test.py
@@ -1,40 +1,22 @@
-import pytest
 from selenium import webdriver
-from selenium.webdriver.firefox.options import Options as FirefoxOptions
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import Select
-import time
-from selenium.webdriver.firefox.service import Service
-from selenium.webdriver.firefox.options import Options
-import pytest
-from selenium import webdriver
-from selenium.webdriver.firefox.options import Options
-from selenium.webdriver.firefox.service import Service
-from selenium.webdriver.firefox.service import Service
-from webdriver_manager.firefox import GeckoDriverManager
-from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import Select 
+import pytest
 import logging
 import os
 
 @pytest.fixture
 def driver():
     options = Options()
-    options.add_argument("--headless")
+    options.add_argument('--headless')
     options.add_argument('--no-sandbox')
-    options.add_argument('--disable-dev-shm-usage')
-    options.add_argument('--disable-gpu')
-    options.add_argument('--window-size=1920,1080')
-    options.log_level = "trace"
-    options.set_preference("webdriver.log.file", "/tmp/geckodriver.log")
-
-    service = Service(GeckoDriverManager().install())
-
-    driver = webdriver.Firefox(service=service, options=options)
-    driver.set_page_load_timeout(30)
-    driver.get("http://127.0.0.1:2000")
-
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
     yield driver
     driver.quit()
     
